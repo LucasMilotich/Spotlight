16cb270bc91cfdd6757c5267374a2d163d858e38
Lucas Milotich <liucas.milotich@gmail.com>
2016-06-12 15:17:42 -0300

    modelo

--- src/com/hera/entities/Tapa.java
+++ src/com/hera/entities/Tapa.java
@@ -1,5 +1,120 @@
 package com.hera.entities;
 
 public class Tapa {
+	
+	private String nombreTapa;
+	
+	private int tamanioTapaX;
+	private int tamanioTapaY;
+	private int tamanioFotoX;
+	private int tamanioFotoY;
+	
+	private int posicionTapaX;
+	private int posicionTapaY;
+	private int posicionFotoX;
+	
+	private String fuente;
+	private String tamanio;
+	
+	private String pathTapa;
+	private String pathFoto;
+	private String categoria;
+	private String pais;
+	
+	public String getNombreTapa() {
+		return nombreTapa;
+	}
+	public void setNombreTapa(String nombreTapa) {
+		this.nombreTapa = nombreTapa;
+	}
+	public int getTamanioTapaX() {
+		return tamanioTapaX;
+	}
+	public void setTamanioTapaX(int tamanioTapaX) {
+		this.tamanioTapaX = tamanioTapaX;
+	}
+	public int getTamanioTapaY() {
+		return tamanioTapaY;
+	}
+	public void setTamanioTapaY(int tamanioTapaY) {
+		this.tamanioTapaY = tamanioTapaY;
+	}
+	public int getTamanioFotoX() {
+		return tamanioFotoX;
+	}
+	public void setTamanioFotoX(int tamanioFotoX) {
+		this.tamanioFotoX = tamanioFotoX;
+	}
+	public int getTamanioFotoY() {
+		return tamanioFotoY;
+	}
+	public void setTamanioFotoY(int tamanioFotoY) {
+		this.tamanioFotoY = tamanioFotoY;
+	}
+	public int getPosicionTapaX() {
+		return posicionTapaX;
+	}
+	public void setPosicionTapaX(int posicionTapaX) {
+		this.posicionTapaX = posicionTapaX;
+	}
+	public int getPosicionTapaY() {
+		return posicionTapaY;
+	}
+	public void setPosicionTapaY(int posicionTapaY) {
+		this.posicionTapaY = posicionTapaY;
+	}
+	public int getPosicionFotoX() {
+		return posicionFotoX;
+	}
+	public void setPosicionFotoX(int posicionFotoX) {
+		this.posicionFotoX = posicionFotoX;
+	}
+	public int getPosicionFotoY() {
+		return posicionFotoY;
+	}
+	public void setPosicionFotoY(int posicionFotoY) {
+		this.posicionFotoY = posicionFotoY;
+	}
+	public String getFuente() {
+		return fuente;
+	}
+	public void setFuente(String fuente) {
+		this.fuente = fuente;
+	}
+	public String getTamanio() {
+		return tamanio;
+	}
+	public void setTamanio(String tamanio) {
+		this.tamanio = tamanio;
+	}
+	public String getPathTapa() {
+		return pathTapa;
+	}
+	public void setPathTapa(String pathTapa) {
+		this.pathTapa = pathTapa;
+	}
+	public String getPathFoto() {
+		return pathFoto;
+	}
+	public void setPathFoto(String pathFoto) {
+		this.pathFoto = pathFoto;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	private int posicionFotoY;
+	
+	
+	
+	
 
 }
---
+++ src/com/hera/imageProcessing/ImageProcessing.java
@@ -0,0 +1,5 @@
+package com.hera.imageProcessing;
+
+public class ImageProcessing {
+
+}
