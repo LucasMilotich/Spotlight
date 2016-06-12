2300bc429d9213c9136686f7afae90f1b4fd992e
anicolasgar <a.nicolasgar@gmail.com>
2016-06-12 17:36:58 -0300

    Fixed cosas menores

--- src/com/hera/entities/Foto.java
+++ src/com/hera/entities/Foto.java
@@ -5,57 +5,62 @@ import java.io.File;
 import android.graphics.Bitmap;
 
 
-
 public class Foto {
-	
-	// caracteristicas tomadas del xml
-	private int tamanioFotoX;
-	private int tamanioFotoY;
-	
-	private File pathFoto;
-	private Bitmap foto;
-	private int posicionFotoX;
-	private int posicionFotoy;
-	
-	  public Foto(int tamanioFotoX, int tamanioFotoY, int posicionFotoX, int posicionFotoy, File pathFoto, Bitmap foto) {
-	        this.tamanioFotoX = tamanioFotoX;
-	        this.tamanioFotoY = tamanioFotoY;
-	        this.posicionFotoX = posicionFotoX;
-	        this.posicionFotoy = posicionFotoy;
-	        this.pathFoto = pathFoto;
-	        this.foto = foto;
-	    }
-	  
-	public Foto(Bitmap foto){
-		this.foto = foto;
-	}
-	public int getTamanioFotoX() {
-		return tamanioFotoX;
-	}
-	public void setTamanioFotoX(int tamanioFotoX) {
-		this.tamanioFotoX = tamanioFotoX;
-	}
-	public int getTamanioFotoY() {
-		return tamanioFotoY;
-	}
-	public void setTamanioFotoY(int tamanioFotoY) {
-		this.tamanioFotoY = tamanioFotoY;
-	}
 
-	public File getPathFoto() {
-		return pathFoto;
-	}
-	public void setPathFoto(File pathFoto) {
-		this.pathFoto = pathFoto;
-	}
-	
-	
-	public Bitmap getFoto() {
-		return foto;
-	}
-	public void setFoto(Bitmap foto) {
-		this.foto = foto;
-	}
-	
-	
+    // caracteristicas tomadas del xml
+    private int tamanioFotoX;
+    private int tamanioFotoY;
+
+    private File pathFoto;
+    private Bitmap foto;
+    private int posicionFotoX;
+    private int posicionFotoy;
+
+    public Foto(int tamanioFotoX, int tamanioFotoY, int posicionFotoX, int posicionFotoy, File pathFoto, Bitmap foto) {
+        this.tamanioFotoX = tamanioFotoX;
+        this.tamanioFotoY = tamanioFotoY;
+        this.posicionFotoX = posicionFotoX;
+        this.posicionFotoy = posicionFotoy;
+        this.pathFoto = pathFoto;
+        this.foto = foto;
+    }
+
+    public Foto(Bitmap foto) {
+        this.foto = foto;
+    }
+
+    public int getTamanioFotoX() {
+        return tamanioFotoX;
+    }
+
+    public void setTamanioFotoX(int tamanioFotoX) {
+        this.tamanioFotoX = tamanioFotoX;
+    }
+
+    public int getTamanioFotoY() {
+        return tamanioFotoY;
+    }
+
+    public void setTamanioFotoY(int tamanioFotoY) {
+        this.tamanioFotoY = tamanioFotoY;
+    }
+
+    public File getPathFoto() {
+        return pathFoto;
+    }
+
+    public void setPathFoto(File pathFoto) {
+        this.pathFoto = pathFoto;
+    }
+
+
+    public Bitmap getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Bitmap foto) {
+        this.foto = foto;
+    }
+
+
 }
